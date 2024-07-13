6381a47626a9ad1fe0239ad9857271f8995b3000
Prabhjot kaur <prabhjotkkmander05@gmail.com>
2024-07-13 10:05:45 -0400

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee employee = new Employee("Prabh", "kaur", 58,19);
+
+        System.out.println(employee);
+
+        employee.getRaise();
+        System.out.println("New hourly pay after raise: $" + employee.getHourlyPay());
+
+        double totalPay = employee.payDay(45);
+        System.out.println("Total pay for the week: $" + totalPay);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,7 @@
 public class Person {
-    private  String firstName;
-    private  String lastName;
-    private double height;
+    private String firstName;
+    private String lastName;
+    protected double height;
 
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
@@ -16,20 +16,62 @@ public class Person {
         return lastName;
     }
 
-    private double getHeight(){
+    public double getHeight() {
         return height;
     }
 
-    public void setFirstName(String firstName){
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public void setLastName(String lastName){
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    private void setHeight(double height){
-        this.height=height;
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
+
+ class Employee extends Person {
+    private int id;
+    private double hourlyPay;
+
+    public Employee(String firstName, String lastName, int id, double hourlyPay) {
+        super(firstName, lastName);
+        this.id = id;
+        this.hourlyPay = hourlyPay;
     }
 
+    // Getters and setters for id and hourlyPay
+    public int getId() {
+        return id;
+    }
+
+     public double getHourlyPay() {
+         return hourlyPay;
+     }
+
+    public double getRaise() {
+        hourlyPay *= 1.15;
+        return hourlyPay;
+    }
+
+    public double payDay(double hoursWorked) {
+        double overtimeHours = Math.max(0, hoursWorked - 40);
+        double regularHours = Math.min(hoursWorked, 40);
+        double overtimePay = overtimeHours * hourlyPay * 1.5;
+        double regularPay = regularHours * hourlyPay;
+        return overtimePay + regularPay;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + "\n" +
+                "Hourly Pay: $" + hourlyPay + "\n" +
+                "Employee ID: " + id;
+    }
 }
+
+
+
+
